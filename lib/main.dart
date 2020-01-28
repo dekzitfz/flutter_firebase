bf60783e6ca4921303b7db74f390eb6f9f14257d
Adi Andrea <dekz.itfz@gmail.com>
2020-01-28 12:39:19 +0800

    add google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 void main() {
   runApp(new MaterialApp(
@@ -24,6 +26,34 @@ class _State extends State<MyApp> {
   void initState() {
     super.initState();
     _status = 'Not Authenticated';
+  }
+
+  void _handleGoogleLogin() {
+    _signWithGoogle()
+        .then((FirebaseUser user){
+          setState(() {
+            _status = 'Loged as ${user.email}';
+          });
+        })
+        .catchError((e){
+          print(e.toString());
+          setState(() {
+            _status = 'Login Failed';
+          });
+        });
+  }
+
+  Future<FirebaseUser> _signWithGoogle() async {
+    final GoogleSignInAccount googleAccount = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken
+    );
+
+    final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+    return user;
   }
 
   void _loginAnonymously() async {
@@ -56,21 +86,21 @@ class _State extends State<MyApp> {
         padding: new EdgeInsets.all(10.0),
         child: new Center(
           child: new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               new Text("Status: $_status"),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  new RaisedButton(
-                    onPressed: _logout,
-                    child: new Text("Logout"),
-                  ),
-                  new RaisedButton(
-                    onPressed: _loginAnonymously,
-                    child: new Text("Login as anon"),
-                  )
-                ],
+              new RaisedButton(
+                onPressed: _logout,
+                child: new Text("Logout"),
+              ),
+              new RaisedButton(
+                onPressed: _loginAnonymously,
+                child: new Text("Login as anon"),
+              ),
+              new RaisedButton(
+                onPressed: _handleGoogleLogin,
+                child: new Text("Login with google"),
               )
             ],
           ),
